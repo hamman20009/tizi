e9fb09bbb919e00d4aabc46f4b8276ff5bbedd40
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-06-25 05:11:50 +0200

    Update products.js

--- products.js
+++ products.js
@@ -3,9 +3,6 @@ const products = [
 		id: 2,
 		logo:'img/logo.png',
 		image: 'img/1.png',
-		
-		image: 'img/2.png',
-		image: 'img/3.png',
 		title: 'اكياس لتخفيض نسبة السكر في الدم(50 كيس)    '  ,
 		price1: 3500,
 		price: 3000,
